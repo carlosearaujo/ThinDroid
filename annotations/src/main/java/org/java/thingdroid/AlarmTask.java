eef9bc5f1839c71a1b742ef6c46c45b459a7f3a5
Carlos E Araujo <carlos.araujo@trixlog.com>
2016-03-01 09:51:04 -0300

    Annotations module

---
+++ annotations/src/main/java/org/java/thingdroid/AlarmTask.java
@@ -0,0 +1,21 @@
+package org.java.thingdroid;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Carlos on 23/12/2015.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AlarmTask{
+
+    long MINUTE = 60 * 1000, FIVE_MINUTES = 5* MINUTE, THIRTY_MINUTES = 30 * MINUTE, ONE_HOUR = 60 * MINUTE, DAY = 24 *ONE_HOUR, INFINITE = -1;
+
+    long interval() default INFINITE;
+    boolean wakeUp() default false;
+
+    long maxLazyWait() default INFINITE;
+}
---
+++ annotations/src/main/java/org/java/thingdroid/AnnotationResolver.java
@@ -0,0 +1,8 @@
+package org.java.thingdroid;
+
+/**
+ * Created by Carlos on 23/12/2015.
+ */
+public interface AnnotationResolver {
+    Class[] getManagedClasses();
+}
